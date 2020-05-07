763134281a57ca635912b107453c2b33e57f69e1
Ilya Mironchikov <i.mironchikov@gmail.com>
2020-05-08 01:04:16 +0300

    images

--- app/src/Controller/IndexController.php
+++ app/src/Controller/IndexController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use GuzzleHttp\Client;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController
+class IndexController extends AbstractController
 {
 
     /**
@@ -17,5 +19,17 @@ class IndexController
         $result = 'index page';
         return new Response($result);
 
+    }
+
+    /**
+     * @Route("/upload", name="upload")
+     * @param Request $request
+     * @return Response
+     */
+    public function upload(Request $request){
+        if ($request->files->get('image')){
+            dd($request->files->get('image'));
+        }
+        return $this->render('index/index.html.twig', []);
     }
 }
